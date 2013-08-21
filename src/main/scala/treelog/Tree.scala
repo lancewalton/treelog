78a137007d58454cb61ec65501844a1d2f5996c4
lancewalton <lancewalton@mac.com>
2013-08-21 15:55:09 +0100

    Added map function

--- src/main/scala/treelog/Tree.scala
+++ src/main/scala/treelog/Tree.scala
@@ -1,5 +1,12 @@
 package treelog
 
-sealed trait Tree[+Label]
-case object NilTree extends Tree[Nothing]
-case class TreeNode[Label](label: Label, children: List[Tree[Label]] = Nil) extends Tree[Label]
+sealed trait Tree[+Label] {
+  def map[A](f: Label ⇒ A): Tree[A]
+}
+case object NilTree extends Tree[Nothing] {
+  def map[A](f: Nothing ⇒ A): Tree[A] = NilTree
+}
+
+case class TreeNode[Label](label: Label, children: List[Tree[Label]] = Nil) extends Tree[Label] {
+  def map[A](f: Label ⇒ A): Tree[A] = TreeNode(f(label), children.map(_.map(f)))
+}
