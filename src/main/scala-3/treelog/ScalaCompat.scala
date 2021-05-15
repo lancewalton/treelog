0ea31c427f15c94f84b4750c84a2e0b8ff1eaead
Channing Walton <channingwalton@mac.com>
2021-05-15 15:27:41 +0100

    trying scala 3 braceless

--- src/main/scala-3/treelog/ScalaCompat.scala
+++ src/main/scala-3/treelog/ScalaCompat.scala
@@ -1,14 +1,9 @@
 package treelog
 
-private[treelog] object ScalaCompat {
+private[treelog] object ScalaCompat:
 
-  implicit class LazyListCompanionOps(private val lc: LazyList.type) {
+  implicit class LazyListCompanionOps(private val lc: LazyList.type):
     def apply[T](xs: T*): LazyList[T] = xs.toLazyList
-  }
 
-  implicit class IterableOps[T](private val iterable: Iterable[T])
-      extends AnyVal {
+  implicit class IterableOps[T](private val iterable: Iterable[T]) extends AnyVal:
     def toLazyList: LazyList[T] = iterable.to(LazyList)
-  }
-
-}
